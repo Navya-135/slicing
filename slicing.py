1df4c4948a4ab970dd1d4c676b689df5438d469d
Navya-135 <143674020+Navya-135@users.noreply.github.com>
2023-10-13 13:14:38 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,28 @@
+#String slicing
+
+name="Rajesh"
+print(name[0:2])
+print(name[:])
+print(name[:5])
+print(name[2:])
+
+
+#reverse a string
+
+name="Navya"
+print(name[::-1])
+
+#reverse a string without slicing
+
+name="Malleswari"
+name="".join(reversed(name))
+print(name)
+
+#reverse a list items
+
+Attendees:list=["Rajesh","aravind","sagar"]
+print(Attendees[:-1])
+print(Attendees[:])
+print(Attendees[-1:-4])
+print(Attendees[-2:])
+
